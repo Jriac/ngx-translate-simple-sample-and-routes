42dd22610a1499f79d3c474ed0422e17a57b0d65
Jriac <jriac91@gmail.com>
2019-07-19 08:06:25 +0200

    adds other module for example pourposes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,17 @@
-import { NgModule, Provider,Directive } from '@angular/core';
+import { NgModule, Provider, Directive } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { TranslationComponent } from './translation/translation.component';
-
+import { OtherComponent } from './other-module/other.component'
 
 import { ViewsRoutingModule } from './views-routing.module';
 
 
 @NgModule({
-  imports: [BrowserModule, FormsModule,ViewsRoutingModule],
-  declarations: [AppComponent, TranslationComponent],
+  imports: [BrowserModule, FormsModule, ViewsRoutingModule],
+  declarations: [AppComponent, TranslationComponent, OtherComponent],
   bootstrap: [AppComponent],
   providers: []
 })
--- src/app/views-routing.module.ts
+++ src/app/views-routing.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-
+import {OtherComponent} from './other-module/other.component'
 import { TranslationComponent } from './translation/translation.component';
 
 
 const routes: Routes = [
   { path: 'landing', component: TranslationComponent, data: { header: false, footer: true } },
+  { path: 'other', component: OtherComponent, data: { header: false, footer: true } },
   { path: '', redirectTo: '/landing', pathMatch: 'full' },
   { path: '**', component: TranslationComponent }
 ];

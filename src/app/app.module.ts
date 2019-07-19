bf9ef73ff6665dcdba57f2f5a9e6832da77cbece
Jriac <jriac91@gmail.com>
2019-07-19 08:23:22 +0200

    adds module config

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,22 @@
 import { NgModule, Provider, Directive } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
-
+import {HttpClientModule, HttpClient} from '@angular/common/http';
 import { AppComponent } from './app.component';
 import { TranslationComponent } from './translation/translation.component';
-import { OtherComponent } from './other-module/other.component'
-
+import { OtherComponent } from './other-module/other.component';
+import {TranslateModule} from '@ngx-translate/core';
 import { ViewsRoutingModule } from './views-routing.module';
-
+import {TranslateHttpLoader} from '@ngx-translate/http-loader';
 
 @NgModule({
-  imports: [BrowserModule, FormsModule, ViewsRoutingModule],
+  imports: [BrowserModule, FormsModule, ViewsRoutingModule,TranslateModule.forRoot({
+            loader: {
+                provide: TranslateHttpLoader,
+                useFactory: HttpClient,
+                deps: [HttpClient]
+            }
+        })],
   declarations: [AppComponent, TranslationComponent, OtherComponent],
   bootstrap: [AppComponent],
   providers: []
--- src/app/other-module/other.component.ts
+++ src/app/other-module/other.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {TranslateService} from '@ngx-translate/core';
 
 @Component({
   selector: 'app-other-module',
@@ -7,9 +8,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class OtherComponent implements OnInit {
 
-  constructor() { }
+  constructor(translate: TranslateService) { 
+    translate.setDefaultLang('en');
+    translate.use('en');
+  }
 
   ngOnInit() {
+  
+  
   }
 
 }
